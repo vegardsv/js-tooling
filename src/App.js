9ef94b708fd115d84194770bc7301992f3846a1e
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro.local>
2018-11-12 22:42:06 +0100

    post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animals: [],
+      input: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_API_URL}/animals`)
+      .then(response => response.json())
+      .then(json =>
+        this.setState({
+          animals: json
+        })
+      );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            fetch(`${process.env.REACT_APP_API_URL}/animals`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ animal: this.state.input })
+            })
+              .then(() => fetch(`${process.env.REACT_APP_API_URL}/animals`))
+              .then(response => response.json())
+              .then(json =>
+                this.setState({
+                  animals: json
+                })
+              );
+
+            this.setState({ input: "" });
+          }}
+        >
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={e => this.setState({ input: e.target.value })}
+          />
+          <input type="submit" value="+" />
+        </form>
+
+        <ul>
+          {this.state.animals.map(({ id, animal }) => (
+            <li key={id}>{animal}</li>
+          ))}
+        </ul>
       </div>
     );
   }
